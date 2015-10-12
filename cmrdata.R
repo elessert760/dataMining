3617835711a51c5e9268bb338abafebf783777c6
elessert760 <elessert760@users.noreply.github.com>
2015-10-12 13:29:59 -0700

    Create cmrdata.R

---
+++ cmrdata.R
@@ -0,0 +1,42 @@
+####CMR Web Scrape of Html content
+library(rvest)
+
+CMR <- function(object){
+  
+  setwd("C:/Users/Eric/Documents/R/DataMining/SEC_data/CMR_results")
+  
+  cmr_data <- read.csv("cmr_links_1.csv")
+  
+  dat = data.frame()
+  
+  for (i in object){ 
+    
+    
+    name <- html(as.character(i))
+    
+    returned_name = as.data.frame(name %>%
+                                    html_nodes("div, font") %>%
+                                    html_text())
+    
+    rep_label <- rep(as.character(i), times = nrow(returned_name))
+    
+    bound_values <- as.data.frame(cbind(rep_label, returned_name))     
+    
+    dat = as.data.frame(rbind(bound_values, dat))
+    
+  } 
+  
+  dat = as.data.frame(unique(dat), rownames = F)
+  
+  colnames(dat) <- c("CMR.Link", "Text")
+  
+  dat = merge(cmr_data, dat, by = "CMR.Link")
+  
+  View(dat)
+
+  write.csv(dat, "cmr_20150619_div_font.csv") 
+  
+  
+}
+
+CMR(cmr_data[,2])
